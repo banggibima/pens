c7ba54d4ffe9965134a344d61d8be0a67c309deb
Banggi Bima <banggibima@gmail.com>
2024-08-22 10:31:09 +0700

    chore: adjust main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,15 @@
-"use client";
+import type { Metadata } from "next";
 
-import { useEffect, useState } from "react";
+export const metadata: Metadata = {
+  title: "Home",
+  description: "Home page",
+};
 
 export default function Page() {
-  const [message, setMessage] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch("/api/hello");
-      const { message } = await res.json();
-      setMessage(message);
-    };
-    fetchData();
-  }, []);
-
-  if (!message) return <p>Loading...</p>;
-
   return (
-    <div className="mx-auto max-w-xl">
-      <div className="flex flex-col h-screen bg-neutral-50 dark:bg-neutral-950 selection:bg-neutral-100 dark:selection:bg-neutral-900">
-        <div className="flex flex-col justify-center items-start h-full"></div>
+    <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
+      <div className="mx-auto max-w-xl w-full px-0 sm:px-0 lg:px-0">
+        <div className="flex flex-col gap-y-4 items-center justify-center"></div>
       </div>
     </div>
   );
